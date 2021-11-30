531c67f2e50ac4bbdcf5f6452819734e665f0b64
goharm <mrgoharm@gmail.com>
2021-11-30 12:26:14 +0900

    키보드를 통한 방향전환

--- move_controlwithkey.py
+++ move_controlwithkey.py
@@ -1,12 +1,4 @@
 from djitellopy import Tello
-import controller as ctr
-
-
-tello  = Tello()
-
-tello.connect()
-
-tello.takeoff()
 
 # # 이동
 # tello.move_up(100)
@@ -28,10 +20,52 @@ tello.takeoff()
 
 
 
-try:
-    tello.land()
-except Exception as e:
-    pass
+# try:
+#     tello.land()
+# except Exception as e:
+#     pass
 
+
+tello  = Tello()
+tello.connect()
+
+import cv2 as cv
+
+panel = cv.imread('./mqdefault.jpg')
+cv.imshow('tello control panel', panel)
+
+move_val = 50
+
+while True:
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+    elif key == ord('t'):
+        tello.takeoff()
+    elif key == ord('l'):
+        tello.land()
+    
+    
+    # 업 다운
+    elif key == ord('x'):
+        tello.move('up',move_val)
+    elif key == ord('z'):
+        tello.move('down',move_val)
+    #전후 좌우
+    elif key == ord('w'):
+        tello.move('forward',move_val)
+    elif key == ord('a'):
+        tello.move('left',move_val)
+    elif key == ord('d'):
+        tello.move('right',move_val)
+    elif key == ord('s'):
+        tello.move('back',move_val)
+    
+    # 좌/우 회전
+    elif key == ord('e'):
+        tello.rotate_clockwise(15)
+    elif key == ord('q'):
+        tello.rotate_counter_clockwise(15)
 
 pass
